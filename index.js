b37f981a8585edd578cc3657a832be3130d4a1ee
Harsh Prakash <harsh.prakashdps@gmail.com>
2024-05-18 19:26:34 +0530

    Embedding and storing the vectors in supabase

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { compile } from "html-to-text";
 import { RecursiveUrlLoader } from "langchain/document_loaders/web/recursive_url";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import dotenv from "dotenv";
+dotenv.config();
 
 const url = "https://lbb.in/delhi";
 
@@ -8,7 +13,7 @@ const compiledConvert = compile({ wordwrap: 130 }); // returns (text: string) =>
 
 const loader = new RecursiveUrlLoader(url, {
   extractor: compiledConvert,
-  maxDepth: 1000,
+  maxDepth: 1,
   excludeDirs: [
     "https://lbb.in/mumbai",
     "https://lbb.in/bangalore",
@@ -20,7 +25,24 @@ const loader = new RecursiveUrlLoader(url, {
 
 const docs = await loader.load();
 
-const splitter = new RecursiveCharacterTextSplitter();
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  separators: ["\n\n", "\n", " ", ""],
+  chunkOverlap: 50,
+});
 const output = await splitter.splitDocuments(docs);
 
-console.log(output);
+const sbApiKey = process.env.SUPABASE_API_KEY;
+const sbUrl = process.env.SUPABASE_URL;
+const openAIApiKey = process.env.OPENAI_API_KEY;
+
+const client = createClient(sbUrl, sbApiKey);
+
+await SupabaseVectorStore.fromDocuments(
+  output,
+  new OpenAIEmbeddings({ openAIApiKey }),
+  {
+    client,
+    tableName: "documents",
+  }
+);
